c409cfffcf927ece8a88d2c7c64595d87590f06b
Tony Kipkemboi <tonykip@seas.upenn.edu>
2020-10-20 01:09:55 -0400

    Update selenium_web_access.py

--- selenium_web_access.py
+++ selenium_web_access.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 # set path to chrome webdriver in OS
 PATH = "C:\Program Files (x86)\chrome driver\chromedriver.exe"
@@ -11,6 +12,9 @@ driver.get("https://www.youtube.com")
 
 # print title
 print(driver.title)
+
+# delay for 5 seconds 
+time.sleep(5)
 
 # quit
 driver.quit()
